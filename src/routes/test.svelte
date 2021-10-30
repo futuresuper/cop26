4bfcce3377f7d4f87f68ff48801c08ebb11a522e
Andrew <andrew@myfuturesuper.com.au>
2021-10-30 12:48:47 +1100

    feat: test mobile download 7

---
+++ src/routes/test.svelte
@@ -0,0 +1,62 @@
+<script>
+	import { onMount } from 'svelte';
+
+	let text = 'Too soon';
+	let theme = 'none';
+
+	onMount(async () => {
+		text = 'It worked';
+		theme = 'ball';
+	});
+</script>
+
+<section>
+	Original Tile
+	<div id="image-tile" class="tile {theme}">
+		<div class="spacer" />
+		<div class="text">{text}</div>
+		<div class="hashtag">#NOTMUTEONCLIMATE</div>
+	</div>
+</section>
+
+<style lang="scss">
+	section {
+		color: white;
+	}
+
+	.tile {
+		height: 100%;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		align-items: center;
+		text-align: center;
+		font-family: var(--knockout);
+		padding: 2vw;
+		user-select: none;
+		text-transform: uppercase;
+		line-height: 90%;
+
+		&.ball {
+			background-image: url('/images/ball.png');
+			background-size: cover;
+			color: black;
+		}
+	}
+
+	#image-tile {
+		width: 200px;
+		height: 200px;
+		padding: 10px;
+		.text {
+			font-size: 25px;
+			line-height: 100%;
+		}
+		.hashtag {
+			font-size: 12px;
+		}
+		.spacer {
+			height: 12px;
+		}
+	}
+</style>
